e9872e733b7f9af47690a083edcef9c3e806b7e3
salvatorecalo <salvatorecalo4@gmail.com>
2023-09-30 11:36:04 +0200

    fix: changed nuka carousel with splidejs

--- app/sections/Feedback/components/FeedbackCarousel/FeedbackCarousel.jsx
+++ app/sections/Feedback/components/FeedbackCarousel/FeedbackCarousel.jsx
@@ -1,34 +1,56 @@
 'use client'
 import {useFeedbackCarousel} from "@/app/sections/Feedback/components/FeedbackCarousel/hooks/useFeedbackCarousel";
+import {Splide, SplideSlide} from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
 import Card from "@/app/sections/Feedback/components/FeedbackCard/FeedbackCard";
-import Carousel from "nuka-carousel";
 import './FeedbackCarousel.css'
+import {AutoScroll} from "@splidejs/splide-extension-auto-scroll";
 export function FeedbackCarousel(){
     const {feedbacks} = useFeedbackCarousel()
 
     return (
-        <Carousel
-            autoplay
-            adaptiveHeight={true}
-            autoplayInterval={3000}
-            slidesToShow={1}
-            cellAlign="center"
-            dragging={true}
-            enableKeyboardControls={true}
-            wrapAround={true}
+        <Splide
+            options={{
+                width: "100%",
+                rewind: true,
+                autoplay: "true",
+                perPage: 2,
+                perMove: "1",
+                gap: "2rem",
+                height: "15rem",
+                type: "loop",
+                rewindSpeed: "3000",
+                arrows: "true",
+                pagination: "false",
+                extensions: { AutoScroll },
+                autoScroll: {
+                    speed: 3,
+                    pauseOnHover: true,
+                    pauseOnFocus: false,
+                },
+                breakpoints: {
+                    1000: {
+                        perPage: 1,
+                    },
+                },
+            }}
         >
             {
-                feedbacks?.map(feedback => {
-                    return <Card
-                        key={feedback.id}
-                        author={feedback.author}
-                        description={feedback.description}
-                        imgSrc={feedback.imageSrc}
-                        alt={feedback.alt}
-                        company={feedback.company}
-                    />
+                feedbacks?.map(feed => {
+                    return (
+                        <SplideSlide>
+                            <Card
+                                key={feed.id}
+                                author={feed.author}
+                                description={feed.description}
+                                company={feed.company}
+                                alt={feed.alt}
+                                imgSrc={feed.imgSrc}
+                            />
+                        </SplideSlide>
+                    )
                 })
             }
-        </Carousel>
+        </Splide>
     )
 }
